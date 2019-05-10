466fa4ed4123d02b5196233ec93c8cf4058f6910
Andrew Au <cshung@gmail.com>
2019-05-10 14:39:48 -0700

    Hacks for xplat

--- src/vm/autotrace.cpp
+++ src/vm/autotrace.cpp
@@ -2,11 +2,7 @@
 
 #ifdef FEATURE_AUTO_TRACE
 
-#ifdef FEATURE_PAL
-#error auto trace is not supported for cross plat
-#endif
-
-#include <windows.h>
+#include "pal.h"
 
 HANDLE auto_trace_event;
 
@@ -26,14 +22,21 @@ void auto_trace_launch()
     STARTUPINFO si;
     ZeroMemory(&si, sizeof(si));
     si.cb = sizeof(STARTUPINFO);
+#ifndef FEATURE_PAL
     si.dwFlags = STARTF_USESHOWWINDOW;
     si.wShowWindow = SW_HIDE;
+#endif
     
     PROCESS_INFORMATION result;
-    const wchar_t* commandFormat = L"C:\\Windows\\System32\\cmd.exe /c run.cmd collect --providers Microsoft-Windows-DotNETRuntime:FFFFFFFFFFFFFFBF -p %d";
+    #ifdef FEATURE_PAL
+    const char16_t* commandFormat = u"/Users/andrewau/git/diagnostics/src/Tools/dotnet-trace/run.sh collect --providers Microsoft-Windows-DotNETRuntime:FFFFFFFFFFFFFFBF -p %d";
+    #else
+    const char16_t* commandFormat = u"C:\\Windows\\System32\\cmd.exe /c run.cmd collect --providers Microsoft-Windows-DotNETRuntime:FFFFFFFFFFFFFFBF -p %d";
+    #endif
     size_t len = wcslen(commandFormat) + 10 + 1;
-    wchar_t* command = new wchar_t[len];
-    wsprintf(command, commandFormat, currentProcessId);
+    char16_t* command = new char16_t[len];
+    _snwprintf_s(command, len, _TRUNCATE, commandFormat, currentProcessId);
+    const char16_t* currentDirectory = u"C:\\Dev\\diagnostics\\src\\Tools\\dotnet-trace";
 
     BOOL code = CreateProcessW(
         /* lpApplicationName    = */ nullptr,
@@ -43,7 +46,7 @@ void auto_trace_launch()
         /* bInheritHandles      = */ false,
         /* dwCreationFlags      = */ CREATE_NEW_CONSOLE,
         /* lpEnvironment        = */ nullptr,
-        /* lpCurrentDirectory   = */ L"C:\\Dev\\diagnostics\\src\\Tools\\dotnet-trace",
+        /* lpCurrentDirectory   = */ currentDirectory,
         /* lpStartupInfo        = */ &si,
         /* lpProcessInformation = */ &result
     );
